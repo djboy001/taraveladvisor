f8ab4c3e7f561cdae3fdcc1ad3c30340b5e36fd5
djboy001 <108181877+djboy001@users.noreply.github.com>
2023-08-18 02:07:19 +0530

    Add files via upload

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key':'9c2696ebd4mshab23e48afe6a43ep14e976jsn436bbf7a0c16',
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
